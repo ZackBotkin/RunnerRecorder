7254782d4f3f6f22081a262dcf2c50c9fd5282f2
Zack Botkin <zackbotkin@gmail.com>
2022-02-27 16:11:41 -0600

    fix so that it can do more than run in a day

--- src/main.py
+++ src/main.py
@@ -15,25 +15,37 @@ class RunnerReader(object):
         for file in os.listdir(self.runs_directory):
             f = open("%s/%s" % (self.runs_directory, file))
             data = json.load(f)
-            self.runs_by_date[data["date"]] = data
+            if data["date"] not in self.runs_by_date:
+                self.runs_by_date[data["date"]] = []
+            self.runs_by_date[data["date"]].append(data)
 
     def get_total(self):
         total=0
-        for date, data in self.runs_by_date.items():
-            total+= float(data["miles"])
+        for date, data_list in self.runs_by_date.items():
+            for data in data_list:
+                total+= float(data["miles"])
         return total
+
+    def print_all_runs(self):
+        for date, data_list in self.runs_by_date.items():
+            for data in data_list:
+                print("%s -- %s miles" % (date, data["miles"]))
 
 
 def main():
 
     parser = argparse.ArgumentParser(description= 'default parser')
     parser.add_argument('--total', help='running totals')
+    parser.add_argument('--print_all_runs', help='print the running files')
     args = parser.parse_args()
 
     DEFAULT_DIRECTORY="C:\\Users\\zackb\\Notes\\runs"
     reader = RunnerReader(DEFAULT_DIRECTORY)
-    total = reader.get_total()
-    print("%f miles run" % total)
+    if args.print_all_runs:
+        reader.print_all_runs()
+    else:
+        total = reader.get_total()
+        print("%f miles run" % total)
 
 
 if __name__ == '__main__':
