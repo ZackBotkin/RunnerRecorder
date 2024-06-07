21f3011079c773e504c4409c9c563d4783b6d023
Zack Botkin <zackbotkin@gmail.com>
2024-06-07 15:46:24 -0500

    import and use SqlLiteQueryRunner

--- runner/src/io/query_runner.py
+++ runner/src/io/query_runner.py
@@ -1,24 +1,7 @@
 import sqlite3
+from query_runner.src.runner import SqlLiteQueryRunner
 
-class QueryRunner(object):
-
-    def __init__(self, config):
-        self.config = config
-        self.database_file_name = "%s\\%s.db" % (
-            self.config.get("database_directory"),
-            self.config.get("database_name")
-        )
-
-    def run_sql(self, sql_str):
-        conn = sqlite3.connect(self.database_file_name)
-        conn.execute(sql_str)
-        conn.commit()
-
-    def fetch_sql(self, sql_str):
-        conn = sqlite3.connect(self.database_file_name)
-        query = conn.execute(sql_str)
-        results = query.fetchall()
-        return results
+class QueryRunner(SqlLiteQueryRunner):
 
     def create_runs_table(self):
         sql_str = "CREATE TABLE runs(date DATE, miles FLOAT, route_name VARCHAR, comment VARCHAR)"
